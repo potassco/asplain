741097386c4eda7be33253d760ef81f524ac2d97
Susana Hahn <susuhahnml@yahoo.com.mx>
2024-11-21 17:40:14 -0600

    Working without clingraph

--- src/asplain/__init__.py
+++ src/asplain/__init__.py
@@ -1,3 +1,40 @@
 """
 The asplain project.
 """
+
+import logging
+import os
+
+from importlib.resources import path
+
+
+from clingo import Control
+from clingo.script import enable_python
+from clingraph.clingo_utils import ClingraphContext  # type: ignore
+from clingraph.graphviz import compute_graphs, render  # type: ignore
+from clingraph.orm import Factbase  # type: ignore
+
+from .utils.logging import get_logger
+
+log = get_logger("main")
+
+
+def viz_explanation(explanation_symbols, directory="out", name_format="explanation.png"):
+    """
+    Visualize explanation using clingraph
+
+    Args:
+        explanation_symbols (List): _description_
+    """
+    fb = Factbase(default_graph="trace")
+    ctl = Control(["--warn=none"])
+    ctx = ClingraphContext()
+    ctl.add("base", [], "\n".join([s + "." for s in explanation_symbols]))
+    with path("asplain.encodings", "clingraph.lp") as clingraph_encoding:
+        ctl.load(str(clingraph_encoding))
+    enable_python()
+    ctl.ground([("base", [])], context=ctx)
+    ctl.solve(on_model=fb.add_model)
+    graphs = compute_graphs(fb)
+    files = render(graphs, view=True, directory=directory, name_format=name_format)
+    log.info("Render saved in %s", files["trace"])
--- src/asplain/__main__.py
+++ src/asplain/__main__.py
@@ -11,7 +11,8 @@ from .transformers.transformer_pipeline import (
     AbductionPipeline,
     ModelSupportPipeline,
 )
-from importlib_resources import files
+from . import viz_explanation
+from importlib.resources import path
 import os
 
 
@@ -34,32 +35,32 @@ def main() -> None:
     abduction_prg = AbductionPipeline().parse_files(domain_file_paths)
     with open(os.path.join(domain_base_path, "out", "abduction.lp"), "w") as f:
         f.write(abduction_prg)
-        log.debug("Abduction encoding saved in " + f.name)
+        log.info("Abduction encoding saved in " + f.name)
 
     support_prg = ModelSupportPipeline().parse_files(domain_file_paths)
     with open(os.path.join(domain_base_path, "out", "support.lp"), "w") as f:
         f.write(support_prg)
-        log.debug("Support encoding saved in " + f.name)
+        log.info("Support encoding saved in " + f.name)
 
     ctl = Control(["0", "--opt-mode=optN"])
     ctl.add("base", [], abduction_prg)
     ctl.add("base", [], support_prg)
     for f in args.explanation_config:
         ctl.load(f.name)
-    base_encoding = files("asplain.encodings").joinpath("base.lp")
-    ctl.load(str(base_encoding))
+    with path("asplain.encodings", "base.lp") as base_encoding:
+        ctl.load(str(base_encoding))
     ctl.ground([("base", [])])
     # TODO save the rules somewhere (the mapping from id to rule)
     contrastive_explanations = []
     with ctl.solve(yield_=True) as handle:
         for m in handle:
-            # TODO check how to avoid having double models
             if not m.optimality_proven:
-                log.debug("Skipping non optimal model")
                 continue
-            contrastive_explanations.append([str(s) for s in m.symbols(atoms=True)])
-            log.debug("----- Expanation %s", m.number)
-            log.debug(m)
+            symbols = [str(s) for s in m.symbols(atoms=True)]
+            contrastive_explanations.append(symbols)
+            log.info("----- Expanation %s", m.number)
+            log.info(m)
+            viz_explanation(symbols, directory=output_dir, name_format=f"contrastive-ex-{m.number}")
 
 
 if __name__ == "__main__":
