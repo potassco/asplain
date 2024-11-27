d51b6cc4f60ff938238526f70baf245997867e39
Brais Muñiz <mc.brais@gmail.com>
2024-11-27 17:28:52 +0100

    Updates model_support_transformer.py for accepting programs with choice rules
    fixes for linting and typecheck:
    	modified:   src/asplain/transformers/_ast_shortcuts.py
    	modified:   src/asplain/transformers/abduction_transformers.py
    	modified:   src/asplain/transformers/custom_transformer.py
    	modified:   src/asplain/transformers/transformer_pipeline.py

--- src/asplain/transformers/abduction_transformers.py
+++ src/asplain/transformers/abduction_transformers.py
@@ -1,3 +1,7 @@
+"""
+Transformers used for generating the 'model support' reification.
+"""
+
 from clingo import ast
 
 from .custom_transformer import CustomTransformer
@@ -6,42 +10,50 @@ WRAPPER_HYPOTHETICAL_PREDICATE_NAME = "hypothetical"
 WRAPPER_MODEL_PREDICATE_NAME = "_model"
 WRAPPER_ABDUCEDS_PREDICATE_NAME = "_abduced"
 
+# Method name "visit_Rule" doesn't conform to snake_case naming style (invalid-name)
+# Method name "visit_Literal" doesn't conform to snake_case naming style (invalid-name)
+# pylint: disable=C0103
+
 
 class AbducedRemovedTransformer(CustomTransformer):
+    """
+    Adds a special literal `not _abduced(rm, Head)` in the body of each rule, allowing the removing of some atoms when
+    abducing.
+    """
 
     def visit_Rule(self, rule: ast.AST) -> ast.AST:
         """
-        Creates a special literal `not _abduced(rm, Head)` in the body of each rule, allowing the removing of some atoms when abducing.
+        Creates a special literal `not _abduced(rm, Head)` in the body of each rule, allowing the removing of some
+        atoms when abducing.
         """
-        if rule.head.ast_type != ast.ASTType.Literal:  # TODO: Which expressions fall here?
+        if rule.head.ast_type != ast.ASTType.Literal:
             return rule
-        else:
-            # Creates new literal for the body (not _abduced(rm, Head))
-            not_removed_literal = ast.Literal(
+        # Creates new literal for the body (not _abduced(rm, Head))
+        not_removed_literal = ast.Literal(
+            location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
+            sign=True,  # Negative Literal
+            atom=ast.Function(
                 location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                sign=True,  # Negative Literal
-                atom=ast.Function(
-                    location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                    name=WRAPPER_ABDUCEDS_PREDICATE_NAME,
-                    arguments=[
-                        ast.Function(
-                            location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                            name="rm",
-                            arguments=list(),
-                            external=False,
-                        ),
-                        rule.head.atom,
-                    ],
-                    external=False,
-                ),
-            )
+                name=WRAPPER_ABDUCEDS_PREDICATE_NAME,
+                arguments=[
+                    ast.Function(
+                        location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
+                        name="rm",
+                        arguments=[],
+                        external=False,
+                    ),
+                    rule.head.atom,
+                ],
+                external=False,
+            ),
+        )
 
-            # Creates the new rule
-            rule = ast.Rule(
-                location=rule.location,
-                head=rule.head,
-                body=[not_removed_literal] + list(rule.body),
-            )
+        # Creates the new rule
+        rule = ast.Rule(
+            location=rule.location,
+            head=rule.head,
+            body=[not_removed_literal] + list(rule.body),
+        )
 
         return rule
 
@@ -52,6 +64,9 @@ class HypotheticalLiteralsTransformer(CustomTransformer):
     """
 
     def visit_Literal(self, literal: ast.AST) -> ast.AST:
+        """
+        Wraps a literal within a hypothetical/1 predicate.
+        """
         if literal.atom.ast_type != ast.ASTType.SymbolicAtom:
             return literal
         # Adds a wrapper predicate to the head of the rule
--- src/asplain/transformers/model_support_transformers.py
+++ src/asplain/transformers/model_support_transformers.py
@@ -1,4 +1,8 @@
-from typing import Union
+"""
+Transformers used for generating the 'model support' reification.
+"""
+
+from typing import Callable, Sequence, Union, Generator
 
 from clingo import Number, ast
 
@@ -17,48 +21,63 @@ PREVENTS_RULE_PREDICATE_NAME = "_prevents"
 
 log = get_logger("main")
 
+# Method name "visit_Rule" doesn't conform to snake_case naming style (invalid-name)
+# pylint: disable=C0103
 
-class WorldVariableSafeTransformer(CustomTransformer):
+
+class WorldVariableSafetyTransformer(CustomTransformer):
+    """
+    Creates a literal `world(World)` in the body of each rule for safety.
+    """
+
     def visit_Rule(self, rule: ast.AST) -> ast.AST:
         """
-        Creates a literal `world(World)` in the body of each rule for safety.
+        Creates a new literal `world(World)` in the body of the given rule.
         """
-        if rule.head.ast_type != ast.ASTType.Literal:  # TODO: Which expressions fall here?
+        if rule.head.ast_type != ast.ASTType.Literal:
             return rule
-        else:
-            # To ignore rules
-            if rule.head.atom.name in [DEPENDS_RULE_PREDICATE_NAME, PREVENTS_RULE_PREDICATE_NAME]:
-                return rule
-            # Creates new literal for the body (not _abduced(rm, Head))
-            world_literal = ast.Literal(
-                location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                sign=False,  # Positive Literal
-                atom=ast.Function(
-                    location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                    name=WORLD_PREDICATE_NAME,
-                    arguments=[
-                        ast.Variable(
-                            location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                            name=WORLD_VARIABLE_NAME,
-                        )
-                    ],
-                    external=False,
-                ),
-            )
 
-            # Creates the new rule
-            rule = ast.Rule(
-                location=rule.location,
-                head=rule.head,
-                body=[world_literal] + list(rule.body),
-            )
+        # To ignore rules
+        if rule.head.atom.name in [DEPENDS_RULE_PREDICATE_NAME, PREVENTS_RULE_PREDICATE_NAME]:
+            return rule
+
+        # Creates new literal for the body (not _abduced(rm, Head))
+        world_literal = ast.Literal(
+            location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
+            sign=False,  # Positive Literal
+            atom=ast.Function(
+                location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
+                name=WORLD_PREDICATE_NAME,
+                arguments=[
+                    ast.Variable(
+                        location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
+                        name=WORLD_VARIABLE_NAME,
+                    )
+                ],
+                external=False,
+            ),
+        )
+
+        # Creates the new rule
+        rule = ast.Rule(
+            location=rule.location,
+            head=rule.head,
+            body=[world_literal] + list(rule.body),
+        )
 
         return rule
 
 
 class ModelLiteralTransformer(CustomTransformer):
+    """
+    Wraps each literal in the body of the rules in a program within a _model/2 predicate.
+    """
+
     @staticmethod
     def _wrap_literal(literal: ast.AST) -> ast.AST:
+        """
+        Wraps the given `literal` in a _model/2 predicate.
+        """
         return ast.Literal(
             location=literal.location,
             sign=literal.sign,  # Positive literal
@@ -73,148 +92,176 @@ class ModelLiteralTransformer(CustomTransformer):
             ),
         )
 
-    def visit_Rule(self, rule: ast.AST) -> ast.Sequence:
-        if rule.head.ast_type != ast.ASTType.Literal:  # TODO: Which expressions fall here?
+    def visit_Rule(self, rule: ast.AST) -> ast.AST:
+        """
+        Returns the rule but wraps every literal in the body in a _model/2 predicate.
+        """
+        if rule.head.ast_type != ast.ASTType.Literal:
             return rule
-        else:
-            # To ignore rules
-            if rule.head.atom.name in [DEPENDS_RULE_PREDICATE_NAME, PREVENTS_RULE_PREDICATE_NAME]:
-                return rule
 
-            new_body = []
-            for literal in rule.body:
-                if (
-                    literal.atom.ast_type == ast.ASTType.SymbolicAtom
-                    and literal.atom.symbol.name != MODEL_WRAPPER_PREDICATE_NAME
-                ):
-                    new_body.append(self._wrap_literal(literal))
-                else:
-                    new_body.append(literal)
+        # To ignore rules
+        if rule.head.atom.name in [DEPENDS_RULE_PREDICATE_NAME, PREVENTS_RULE_PREDICATE_NAME]:
+            return rule
 
-            # Creates the new rule
-            rule = ast.Rule(
-                location=rule.location,
-                head=rule.head,
-                body=new_body,
-            )
+        new_body = []
+        for literal in rule.body:
+            if (
+                literal.atom.ast_type == ast.ASTType.SymbolicAtom
+                and literal.atom.symbol.name != MODEL_WRAPPER_PREDICATE_NAME
+            ):
+                new_body.append(self._wrap_literal(literal))
+            else:
+                new_body.append(literal)
+
+        # Creates the new rule
+        rule = ast.Rule(
+            location=rule.location,
+            head=rule.head,
+            body=new_body,
+        )
 
         return rule
 
 
 class CommentGenerator(GeneratorTransformer):
-    def visit_Rule(self, rule: ast.AST) -> ast.AST:
+    """
+    Generates a comment comprising the original rule.
+    """
+
+    def visit_Rule(self, rule: ast.AST) -> Generator[ast.AST, None, None]:
+        """
+        Generates a comment for the given rule and then yields the original rule.
+        """
         yield ast.Comment(location=rule.location, value="% " + str(rule), comment_type=0)  # ast.CommentType.Line = 0
         yield rule
 
 
-class SupportRuleTransformer(CustomTransformer):
-    def __init__(self):
+class ExplainabilityReifier(GeneratorTransformer):
+    """
+    Generates the Explainability Reification.
+    For each rule, generates:
+        - **Support_rule**: `_sup(RuleID, World, SupportedAtom, VariableValues)` (from 1 to *N*, where *N* is the
+          number of elements in the disjunction or the number of conditional literals inside the head's choice).
+        - **Depends_rule**: `_depends(SupportLiteral, CausesPool)` (from 0 to 1, for each generated support rule).
+        - **Prevents_rule**: `_prevents(SupportLiteral, InhibitorsPool)` (from 0 to 1, for each generated support rule).
+    """
+
+    def __init__(self) -> None:
         self.rule_count = 0
 
-    def visit_Rule(self, rule: ast.AST) -> ast.AST:
+    def _generate_support_rule(self, supported_lit: ast.AST, rule_body: ast.ASTSequence) -> ast.AST:
         """
-        Creates a special literal `not _abduced(rm, Head)` in the body of each rule, allowing the removing of some atoms when abducing.
+        Creates the support rule from an original rule.
         """
-        head_literals = []
-        rules = []
-        if rule.head.ast_type != ast.ASTType.Literal:  # TODO: Which expressions fall here?
-            if rule.head.ast_type != ast.ASTType.Literal:  # choices
-                for r in rule.head.elements:
-                    head_literals.append(r.literal)
-            else:
-                log.warning("Skiped rule: %s", rule)
-                return rule
-        else:
-            head_literals.append(rule.head)
-            # Increments rule count
+        # Increments rule count
+        self.rule_count += 1
 
-        for lit in head_literals:
+        # Creates new head _sup(RuleID, World, SupportedAtom, VariableValues)
+        xclingo_sup_literal = ast.Literal(
+            location=rule_body[0].location if len(rule_body) > 0 else supported_lit.location,
+            sign=False,  # Positive Literal
+            atom=ast.Function(
+                location=rule_body[0].location if len(rule_body) > 0 else supported_lit.location,
+                name=SUPPORT_RULE_PREDICATE_NAME,
+                arguments=[
+                    ast.SymbolicTerm(
+                        location=rule_body[0].location if len(rule_body) > 0 else supported_lit.location,
+                        symbol=Number(self.rule_count),
+                    ),
+                    ast.Variable(
+                        location=rule_body[0].location if len(rule_body) > 0 else supported_lit.location,
+                        name=WORLD_VARIABLE_NAME,
+                    ),
+                    supported_lit,
+                    ast.Function(
+                        location=rule_body[0].location if len(rule_body) > 0 else supported_lit.location,
+                        name="",  # For creating a tuple
+                        arguments=list(collect_free_vars(rule_body, [])),
+                        external=False,
+                    ),
+                ],
+                external=False,
+            ),
+        )
 
-            self.rule_count += 1
+        # Creates the new support rule
+        support_rule = ast.Rule(
+            location=supported_lit.location,
+            head=xclingo_sup_literal,
+            body=rule_body,
+        )
 
-            # Creates new head _sup(RuleID, World, SupportedAtom, VariableValues)
-            xclingo_sup_literal = ast.Literal(
-                location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                sign=False,  # Positive Literal
-                atom=ast.Function(
-                    location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                    name=SUPPORT_RULE_PREDICATE_NAME,
-                    arguments=[
-                        ast.SymbolicTerm(
-                            location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                            symbol=Number(self.rule_count),
-                        ),
-                        ast.Variable(
-                            location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                            name=WORLD_VARIABLE_NAME,
-                        ),
-                        lit,
-                        ast.Function(
-                            location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                            name="",  # For creating a tuple
-                            arguments=list(collect_free_vars(rule.body)),
-                            external=False,
-                        ),
-                    ],
-                    external=False,
-                ),
-            )
+        return support_rule
 
-            # Creates the new rule
-            rules.append(
-                ast.Rule(
-                    location=rule.location,
-                    head=xclingo_sup_literal,
-                    body=rule.body,
-                )
-            )
-
-        return rules
-
-
-class DependenciesTransformer(GeneratorTransformer):
-    def _generate_depends_rule(self, rule: ast.AST, predicate_name, dependency_catcher) -> Union[ast.AST, None]:
-        dependencies = list(dependency_catcher(rule.body))
-        # import pdb; pdb.set_trace()
+    def _generate_dependency_rule(
+        self,
+        support_rule: ast.AST,
+        predicate_name: str,
+        dependency_catcher: Callable[[Sequence[ast.AST]], Generator[ast.AST, None, None]],
+        additional_causes: list[ast.AST],
+    ) -> Union[ast.AST, None]:
+        """
+        Creates a particular dependency rule, from a given support rule.
+        """
+        dependencies = list(dependency_catcher(support_rule.body)) + additional_causes
         if len(dependencies) == 0:
             return None
-        else:
-            dependency_literal = ast.Literal(
-                location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                sign=False,  # Positive Literal
-                atom=ast.Function(
-                    location=rule.body[0].location if len(rule.body) > 0 else rule.head.location,
-                    name=predicate_name,
-                    arguments=[
-                        rule.head,
-                        ast.Pool(rule.head.location, dependencies),
-                    ],
-                    external=False,
-                ),
-            )
 
-            # Creates the new rule
-            return ast.Rule(
-                location=rule.location,
-                head=dependency_literal,
-                body=[rule.head],
-            )
+        dependency_literal = ast.Literal(
+            location=support_rule.body[0].location if len(support_rule.body) > 0 else support_rule.head.location,
+            sign=False,  # Positive Literal
+            atom=ast.Function(
+                location=(support_rule.body[0].location if len(support_rule.body) > 0 else support_rule.head.location),
+                name=predicate_name,
+                arguments=[
+                    support_rule.head,
+                    ast.Pool(support_rule.head.location, dependencies),
+                ],
+                external=False,
+            ),
+        )
 
-    def visit_Rule(self, rule: ast.AST) -> ast.AST:
-        if (
-            (rule.head.ast_type == ast.ASTType.Literal)
-            and (rule.head.atom.ast_type == ast.ASTType.Function)
-            and (rule.head.atom.name == SUPPORT_RULE_PREDICATE_NAME)
-        ):  # TODO: Which expressions fall here?
-            # Yield Support Rule
+        # Creates the new rule
+        return ast.Rule(
+            location=support_rule.location,
+            head=dependency_literal,
+            body=[support_rule.head],
+        )
+
+    def visit_Rule(self, rule: ast.AST) -> Generator[ast.AST, None, None]:
+        """
+        Generates the support rule, and every needed depends and prevents rules.
+        """
+        supported_atoms = []
+        additional_causes = []
+
+        if rule.head.ast_type == ast.ASTType.Aggregate:  # Choices
+            for conditional_lit in rule.head.elements:
+                supported_atoms.append(conditional_lit.literal)
+                additional_causes.append(list(propagates(conditional_lit.condition)))
+
+        elif rule.head.ast_type == ast.ASTType.Disjunction:  # Disjunctive head
+            log.warning("Disjunction not supported yet, skiped rule: %s", rule)
             yield rule
+
+        elif rule.head.ast_type == ast.ASTType.Literal:  # Non-disjunctive head
+            supported_atoms.append(rule.head)
+            additional_causes.append([])
+        else:
+            raise NotImplementedError(f"Rules with a head of type {rule.head.ASTType} are not supported yet")
+
+        # For each supported atom, we create a new rule.
+        for lit, choice_causes in zip(supported_atoms, additional_causes):
+            support_rule = self._generate_support_rule(lit, rule.body)
+            # Yield Support Rule
+            yield support_rule
             # Yield Depends Rule (if we have dependencies)
-            depends_rule = self._generate_depends_rule(rule, DEPENDS_RULE_PREDICATE_NAME, propagates)
+            depends_rule = self._generate_dependency_rule(
+                support_rule, DEPENDS_RULE_PREDICATE_NAME, propagates, choice_causes
+            )
             if depends_rule is not None:
                 yield depends_rule
             # Yield Prevents Rule (if we have inhibitors)
-            prevents_rule = self._generate_depends_rule(rule, PREVENTS_RULE_PREDICATE_NAME, inhibits)
+            prevents_rule = self._generate_dependency_rule(support_rule, PREVENTS_RULE_PREDICATE_NAME, inhibits, [])
             if prevents_rule is not None:
                 yield prevents_rule
-        else:
-            yield rule
