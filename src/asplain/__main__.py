1191d93f29e201e62b1c707aa7d866765631920e
Hannes Weichelt <hweichelt@uni-potsdam.de>
2024-12-05 16:37:57 +0100

    Added LLM test to main

--- src/asplain/__main__.py
+++ src/asplain/__main__.py
@@ -7,13 +7,22 @@ import sys
 from clingo import clingo_main
 
 from .app import AsplainApp
+from .llm.models import OllamaModel, ModelTag
+
+
+TEST_LLM = True
 
 
 def main() -> None:
     """
     Run the main function.
     """
-    clingo_main(AsplainApp(sys.argv[0]), sys.argv[1:])
+    if TEST_LLM:
+        model = OllamaModel(ModelTag.LLAMA_3_2_1B)
+        response = model.prompt("What's 9 + 10?")
+        print("LLM: ", response)
+    else:
+        clingo_main(AsplainApp(sys.argv[0]), sys.argv[1:])
     sys.exit()
 
 
