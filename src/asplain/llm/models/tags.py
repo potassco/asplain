0f98b57a4ac9374b8039dd5d3635f4db3faf3f08
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-01-20 15:34:09 +0100

    Added OpenAI Model + Tags

--- src/asplain/llm/models/tags.py
+++ src/asplain/llm/models/tags.py
@@ -11,6 +11,7 @@ class Tag:
 
     huggingface: Optional[str] = None
     ollama: Optional[str] = None
+    openai: Optional[str] = None
 
 
 class ModelTag(Enum):
@@ -19,3 +20,5 @@ class ModelTag(Enum):
     LLAMA_3_2 = Tag(huggingface="meta-llama/Llama-3.2-3B", ollama="llama3.2:latest")
     LLAMA_3_2_1B = Tag(huggingface="meta-llama/Llama-3.2-1B", ollama="llama3.2:1b")
     LLAMA_3_3 = Tag(ollama="llama3.3:latest")
+    GPT_4O = Tag(openai="gpt-4o")
+    GPT_4O_MINI = Tag(openai="gpt-4o-mini")
