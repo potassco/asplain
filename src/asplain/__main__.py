cbff11e2c6eb7a19d97c2c47b79fe2a7881efe80
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-02-03 10:56:10 +0100

    Load dotenv in main function

--- src/asplain/__main__.py
+++ src/asplain/__main__.py
@@ -2,9 +2,11 @@
 The main entry point for the application.
 """
 
+import os
 import sys
 
 from clingo import clingo_main
+from dotenv import load_dotenv
 
 from .app import AsplainApp
 
@@ -13,6 +15,7 @@ def main() -> None:
     """
     Run the main function.
     """
+    load_dotenv()
     clingo_main(AsplainApp(sys.argv[0]), sys.argv[1:])
     sys.exit()
 
