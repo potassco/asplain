f00c758331f81e15c11721ab05a9333ca2f1622d
Susana Hahn <susuhahnml@yahoo.com.mx>
2025-07-25 21:39:21 -0600

    First version of tags

--- src/asplain/explainers/contrastive_normal.py
+++ src/asplain/explainers/contrastive_normal.py
@@ -1,14 +1,13 @@
 import os
 from importlib.resources import path
-from typing import Callable, Dict, List, Sequence, Tuple
+from typing import Dict, List, Sequence, Tuple
 
 from clingexplaid.transformers import RuleIDTransformer
-from clingo import Control, Function, Number, Symbol, TheoryTermType
+from clingo import Control, Function, Number, Symbol
 from clingo.script import enable_python
 
 # pylint: disable=E0401
 from clingox.reify import ReifiedTheory, ReifiedTheoryTerm, Reifier
-from clingox.theory import evaluate, is_operator
 from clingraph.clingo_utils import ClingraphContext  # type: ignore
 from clingraph.graphviz import compute_graphs, render  # type: ignore
 from clingraph.orm import Factbase  # type: ignore
@@ -20,63 +19,21 @@ from .base_explainer import Explainer
 log = get_logger("main")
 
 
-def _visit_terms(thy: ReifiedTheory, cb: Callable[[ReifiedTheoryTerm], None]):
-    """
-    Visit the terms occuring in the theory atoms of the given theory.
-
-    This function does not recurse into terms.
-    """
-    print("Visiting terms in theory")
-    for atm in thy:
-        for elem in atm.elements:
-            for term in elem.terms:
-                cb(term)
-        cb(atm.term)
-        guard = atm.guard
-        if guard:
-            cb(guard[1])
-
-
-def _term_symbols(term: ReifiedTheoryTerm, ret: Dict[int, Symbol]) -> None:
-    """
-    Represent arguments to theory operators using clingo's `clingo.Symbol`
-    class.
-
-    Theory terms are evaluated using `clingox.theory.evaluate_unary` and added
-    to the given dictionary using the index of the theory term as key.
-    """
-    if term.type == TheoryTermType.Function and is_operator(term.name):
-        _term_symbols(term.arguments[0], ret)
-        if len(term.arguments) >= 2:
-            _term_symbols(term.arguments[1], ret)
-    elif term.index not in ret:
-        ret[term.index] = evaluate(term)
-
-
-def reify(prg: str = "", files: Sequence[str] = None) -> str:
+def reify(prg: str = "") -> str:
     """
     Do the reification using clingox Reifier
     """
-    print("---------Reifying program")
     symbols: List[Symbol] = []
 
     ctl = Control(["--warn=none"])
     reifier = Reifier(symbols.append, reify_steps=False)
     ctl.register_observer(reifier)
     ctl.add("base", [], prg)
-    if files is not None:
-        for f in files:
-            ctl.load(f)
-
     ctl.ground([("base", [])])
 
     theory_symbols: Dict[int, Symbol] = {}
-    print("Reifying theory symbols")
-    print(ReifiedTheory(symbols))
-    _visit_terms(ReifiedTheory(symbols), lambda term: _term_symbols(term, theory_symbols))
 
     for k, v in theory_symbols.items():
-        print(f"Reified theory symbol: {k} -> {v}")
         symbols.append(Function("theory_symbol", [Number(k), v]))
     return "\n".join([f"{str(s)}." for s in symbols])
 
@@ -106,44 +63,28 @@ class ContrastiveExplainer(Explainer):
     def prepare(self, assumptions: Sequence[Tuple[Symbol, bool]]) -> None:
         """ """
 
-        assumptions_as_constraints = "\n\n%%%%%%%%%%%% Assumptions as constraints %%%%%%%%%%%%\n"
-        assumptions_as_constraints += "\n".join([f":- not {str(s)}." for s, b in assumptions if b])
-        assumptions_as_constraints += "\n".join([f":- {str(s)}." for s, b in assumptions if not b])
+        self._rule_tagged_prg = ""
+        transformer = RuleIDTransformer()
+        for f in self._domain_files:
+            self._rule_tagged_prg += transformer.parse_file(f)
 
-        self._reified_prg = reify(assumptions_as_constraints, self._domain_files)
+        assumptions_as_constraints = "\n".join([f":- not {str(s)}." for s, b in assumptions if b])
+        assumptions_as_constraints += "\n".join([f":- {str(s)}." for s, b in assumptions if not b])
+        self._rule_tagged_prg += "\n\n%%%%%%%%%%%% Assumptions as constraints %%%%%%%%%%%%\n"
+        self._rule_tagged_prg += transformer.parse_string(assumptions_as_constraints)
+
+        with open(os.path.join(self._output_dir, "rule_tagged.lp"), "w") as f:
+            f.write(self._rule_tagged_prg)
+
+            log.debug("Transformed program: \n%s", self._rule_tagged_prg)
+            log.info("Transformed encoding saved in " + f.name)
+
+        # TODO add externals to program to make sure negation is not removed when grounding
+
+        self._reified_prg = reify(self._rule_tagged_prg)
         with open(os.path.join(self._output_dir, "reify.lp"), "w") as f:
             f.write(self._reified_prg)
             log.info("Reified program saved in " + f.name)
-        self.viz_reify()
-
-    def get_pg(
-        self,
-    ):
-        ctl_args = ["0", "--warn=none"]
-        ctl = Control(arguments=ctl_args)
-        ctl.add("base", [], self._reified_prg)
-        with path("asplain.encodings", "reify_to_pg.lp") as base_encoding:
-            log.info("Loading encoding: %s", base_encoding)
-            ctl.load(str(base_encoding))
-
-        ctl.ground([("base", [])])
-        with ctl.solve(yield_=True) as handle:
-            for m in handle:
-                self._pg = "\n".join([str(s) + "." for s in m.symbols(shown=True)])
-        if not self._pg:
-            log.error("No program graph found")
-            return
-
-        with open(os.path.join(self._output_dir, "pg.lp"), "w") as f:
-            f.write(self._pg)
-            log.info("Program graph saved in " + f.name)
-
-        extra_prg = """
-        node(N,reference) :- node(N).
-        edge(N,reference) :- edge(N).
-        edge_attr(N,T,V,reference) :- edge(N,T,V).
-        """
-        self.viz_explanation_graph(self._pg + extra_prg, name="pg", draw=["reference"], draw_types=["pg"])
 
     def explain(
         self,
@@ -165,24 +106,24 @@ class ContrastiveExplainer(Explainer):
         """
 
         self.prepare(assumptions)
-        self.get_pg()
-        return True
         log.info("Model: %s", model_symbols)
         log.info(
             "Will explain %s %s",
             ", why  ".join([""] + [str(q) for q in query_include]),
             ", why not".join([""] + [str(q) for q in query_exclude]),
         )
-        # if assumptions is None:
-        #     assumptions = []
-        # log.info("Assumptions: %s", [(str(s), b) for s, b in assumptions])
-        # self.assert_is_model(model_symbols, assumptions)
-
+        if assumptions is None:
+            assumptions = []
+        log.info("Assumptions: %s", [(str(s), b) for s, b in assumptions])
+        self.assert_is_model(model_symbols, assumptions)
         ctl_args = ["0", "--warn=none"]
+
         ctl = Control(arguments=ctl_args)
         constraint_model_prg = "\n".join([f":- not hold({str(s)})." for s in model_symbols])
         ctl.add("base", [], constraint_model_prg)
-        ctl.add("base", [], self._pg)
+        ctl.add("base", [], self._reified_prg)
+
+        ctl.add("base", [], constraint_model_prg)
 
         with path("asplain.encodings", "all_reference.lp") as base_encoding:
             log.info("Loading encoding: %s", base_encoding)
@@ -242,30 +183,8 @@ class ContrastiveExplainer(Explainer):
 
         return contrastive_explanations
 
-    def viz_reify(self) -> None:
-
-        fb = Factbase()
-        ctl = Control(["--warn=none"])
-        ctx = ClingraphContext()
-        ctl.add("base", [], self._reified_prg)
-
-        with path("asplain.encodings", "viz_reify.lp") as clingraph_encoding:
-            ctl.load(str(clingraph_encoding))
-        enable_python()
-        ctl.ground([("base", [])], context=ctx)
-        ctl.solve(on_model=fb.add_model)
-        graphs = compute_graphs(fb, graphviz_type="directed")
-        files = render(graphs, view=True, directory=self._output_dir, name_format="reify", format="svg")
-        for _, f in files.items():
-            log.info("Reify: " + f)
-
     def viz_explanation_graph(
-        self,
-        explanation_graph: str,
-        name: str = "explanation",
-        natural_language_explanation: str = None,
-        draw_types: [str] = None,
-        draw: [str] = None,
+        self, explanation_graph: str, name: str = "explanation", natural_language_explanation: str = None
     ) -> None:
         """
         Visualize the explanation graph using cligraph
@@ -275,20 +194,10 @@ class ContrastiveExplainer(Explainer):
             name: The name of the output file. File will be stored in the same directory
                     as the domain files, inside the `out` directory.
         """
-        if not draw_types:
-            draw_types = []
-        if not draw:
-            draw = []
-
         fb = Factbase(prefix="v_")
         ctl = Control(["--warn=none"])
         ctx = ClingraphContext()
         ctl.add("base", [], explanation_graph)
-
-        for d in draw_types:
-            ctl.add("base", [], f"draw_type({d}).")
-        for d in draw:
-            ctl.add("base", [], f"draw({d}).")
 
         with path("asplain.encodings", "viz_pg.lp") as clingraph_encoding:
             ctl.load(str(clingraph_encoding))
